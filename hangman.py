a81b24251118916c6fb0fd7ec7343f25945b578c
ZipticGladiator <siya.gladile@gmail.com>
2024-06-30 13:24:02 +0200

    added pseudocode

--- hangman.py
+++ hangman.py
@@ -1,1 +1,6 @@
 import random
+
+#create a wordlist to randomly select from
+#if user selects an alphabet that is in the selected word,
+# return the letter in its correct position
+#end the game when all letters are guessed, or when user has guesses len(word) + 2 chances
